b9a10f2bb54046dc8e26f11ef9929f2ef20f8b6e
NguyenVoVuVan_2023 <union1232000@gmail.com>
2024-05-20 00:15:39 +0700

    refactor(problem1): refactor method 3

--- src/problem1/problem1.js
+++ src/problem1/problem1.js
@@ -1,21 +1,24 @@
 // Using for loop
-function sum_to_n_a(n) {
-  let sum = 0
-  for (let i = 1; i <= n; i++) {
-    sum += i
+var sum_to_n_a = function(n) {
+  var sum = 0;
+  for (var i = 1; i <= n; i++) {
+      sum += i;
   }
-  return sum
-}
+  return sum;
+};
+
 
 // Using mathematical formulas
-function sum_to_n_b(n) {
-  return (n * (n + 1)) / 2
-}
+var sum_to_n_b = function(n) {
+  return (n * (n + 1)) / 2;
+};
+
 
 // using recursion
-function sum_to_n_c(n) {
-  if (n <= 1) {
-    return n
+var sum_to_n_c = function(n) {
+  if (n === 0) {
+      return 0;
+  } else {
+      return n + sum_to_n_c(n - 1);
   }
-  return n + sum_to_n(n - 1)
-}
+};
