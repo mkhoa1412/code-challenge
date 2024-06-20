5d6a48b17169e4b5cf746a7e2d82f7bd1bfd90e5
lehai4 <86140878+lehai4@users.noreply.github.com>
2024-06-20 17:49:14 +0700

    problem1 done!

---
+++ src/problem1/task1.js
@@ -0,0 +1,21 @@
+var sum_to_n_a = function (n) {
+  return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_b = function (n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n + sum_to_n_a(n - 1);
+  }
+};
+
+var sum_to_n_c = function (n) {
+  let i = 1;
+  let sum = 0;
+  do {
+    sum += i;
+    i++;
+  } while (i <= n);
+  return sum;
+};
