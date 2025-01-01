43c94c3a26b3bed27294a27134c3096a7f6ee75c
Nhan Truong <trconghiennhan2712@gmail.com>
2025-01-01 19:45:07 +0700

    fix(problem1): provide more console and update logic

--- src/problem1/problem1.ts
+++ src/problem1/problem1.ts
@@ -11,27 +11,33 @@ function sum_to_n_x(n: number): number {
 };
 
 function sum_to_n_y(n: number): number {
+    if (n <= 0) return 0;
+    n = Math.floor(n); // Assume n is 5.99, it will be converted to 5
     return (n * (n + 1)) / 2;
 };
 
 function sum_to_n_z(n: number): number {
-    if (n <= 1) return n;
-    return n + sum_to_n_c(n - 1);
+    if (n <= 0) return 0;
+    n = Math.floor(n); // Assume n is 5.99, it will be converted to 5
+    return n + sum_to_n_z(n - 1);
 };
 
 // sum_to_n_x()
+console.log(sum_to_n_x(-1)) // 0
 console.log(sum_to_n_x(0)) // 0
 console.log(sum_to_n_x(5.99)) // 15
 console.log(sum_to_n_x(5)) // 55
 // console.log(sum_to_n_x(Number.MAX_SAFE_INTEGER))
 
 // sum_to_n_b()
-console.log(sum_to_n_b(0)) // 0
-console.log(sum_to_n_b(5.99)) // 15
-console.log(sum_to_n_b(5)) // 55
-console.log(sum_to_n_b(Number.MAX_SAFE_INTEGER)) // very fast, give the correct result - 4.0564819207303336e+31
+console.log(sum_to_n_y(-1)) // 0
+console.log(sum_to_n_y(0)) // 0
+console.log(sum_to_n_y(5.99)) // 15
+console.log(sum_to_n_y(5)) // 55
+console.log(sum_to_n_y(Number.MAX_SAFE_INTEGER)) // very fast, give the correct result - 4.0564819207303336e+31
 
 // sum_to_n_c()
-console.log(sum_to_n_c(0)) // 0
-console.log(sum_to_n_c(5.99)) // 15
-console.log(sum_to_n_c(5)) // 55
+console.log(sum_to_n_z(-1)) // 0
+console.log(sum_to_n_z(0)) // 0
+console.log(sum_to_n_z(5.99)) // 15
+console.log(sum_to_n_z(5)) // 55
