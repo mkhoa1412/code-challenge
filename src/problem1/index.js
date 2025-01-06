991ec2042db6515a7dba95b7563e0b021ba9ffbf
Linh Nguyen <khtnhonglinh@gmail.com>
2025-01-06 23:03:42 +0700

    feat: implement problem 1 and add note with problem 3

---
+++ src/problem1/index.js
@@ -0,0 +1,24 @@
+// Iterative approach: Uses a for loop to calculate the sum of integers from 1 to n
+var sum_to_n_a = function (n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+// Mathematical formula approach: Uses the arithmetic series sum formula: (n * (n + 1)) / 2
+var sum_to_n_b = function (n) {
+  return (n * (n + 1)) / 2;
+};
+
+// Recursive approach: Uses recursion to calculate the sum of integers from 1 to n
+// This solution is unsafe with stack overflow when the input n is too large
+// In case optimize we should use the Tail-call optimization
+var sum_to_n_c = function (n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  return n + sum_to_n_c(n - 1);
+};
---
+++ src/problem3/README.md
@@ -0,0 +1,9 @@
+# Summary computational inefficiencies and anti-patterns found
+
+- Adjust import module for use
+- Define interface with more inheritance
+- Define magic value as constant variables
+- Binding typescript checking
+- Separate logic into small piece of code and out of render component logic with custom hook
+- Add the fallback render
+- Reduce unnecessary useMemo
