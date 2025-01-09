8cd26b8a25c7258df0ee9f64002b12e36c275b76
Dat Nguyen <dnguyen@3knbh.com>
2025-01-09 20:08:18 +0700

    add  convert functionality

--- src/problem2/src/components/Converter.tsx
+++ src/problem2/src/components/Converter.tsx
@@ -2,6 +2,7 @@ import { useCryptoCurrencies } from "@/api/useCryptoCurrencies";
 import { CurrencySelect } from "@/components/CurrencySelect";
 import { NumberField } from "@/components/NumberField";
 import { Form } from "@/components/ui/form";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface IFormProps {
@@ -23,10 +24,66 @@ export const Converter = () => {
       from: "0",
       to: "0",
       currencyFrom: "ETH",
-      currencyTo: "USDT",
+      currencyTo: "USD",
     },
   });
-  const { control, setValue } = form;
+  const { control, setValue, watch } = form;
+
+  const watchFrom = watch("from");
+  const watchTo = watch("to");
+  const watchCurrencyFrom = watch("currencyFrom");
+  const watchCurrencyTo = watch("currencyTo");
+
+  const [activeField, setActiveField] = useState<"from" | "to" | null>(null);
+
+  const getCurrencyPrice = (currency: string) => {
+    return currencies.find((c) => c.currency === currency)?.price ?? 0;
+  };
+
+  const convertAmount = (
+    amount: number,
+    fromCurrency: string,
+    toCurrency: string,
+  ) => {
+    const fromPrice = getCurrencyPrice(fromCurrency);
+    const toPrice = getCurrencyPrice(toCurrency);
+    if (!fromPrice || !toPrice) return 0;
+
+    return (amount * fromPrice) / toPrice;
+  };
+
+  //Convert FROM -> TO
+  useEffect(() => {
+    console.log(watchFrom);
+    if (activeField !== "from") return;
+
+    const fromNum = parseFloat(watchFrom);
+    if (isNaN(fromNum)) return setValue("to", "0");
+
+    const convertedValue = convertAmount(
+      fromNum,
+      watchCurrencyFrom,
+      watchCurrencyTo,
+    );
+
+    setValue("to", convertedValue.toString());
+  }, [watchFrom, watchCurrencyFrom, watchCurrencyTo, activeField]);
+
+  //Convert TO -> FROM
+  useEffect(() => {
+    if (activeField !== "to") return;
+
+    const toNum = parseFloat(watchTo);
+    if (isNaN(toNum)) return setValue("from", "0");
+
+    const convertedValue = convertAmount(
+      toNum,
+      watchCurrencyTo,
+      watchCurrencyFrom,
+    );
+
+    setValue("from", convertedValue.toString());
+  }, [watchTo, watchCurrencyFrom, watchCurrencyTo, activeField]);
 
   return errorCurrencies ? (
     <div className="text-sm text-red-500">Failed to fetch currencies</div>
@@ -39,6 +96,7 @@ export const Converter = () => {
           placeholder="From"
           inputProps={{
             disabled: loadingCurrencies,
+            onFocus: () => setActiveField("from"),
           }}
         />
 
@@ -57,6 +115,7 @@ export const Converter = () => {
           placeholder="To"
           inputProps={{
             disabled: loadingCurrencies,
+            onFocus: () => setActiveField("to"),
           }}
         />
 
@@ -65,7 +124,7 @@ export const Converter = () => {
           control={control}
           name="currencyTo"
           setValue={setValue}
-          defaultValue={"USDT"}
+          defaultValue={"USD"}
           disabled={loadingCurrencies}
         />
       </form>
