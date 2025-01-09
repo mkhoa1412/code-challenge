7966ae5df488c1eeb62b6e6f67cd2cbe8cba760e
Dat Nguyen <dnguyen@3knbh.com>
2025-01-10 00:06:33 +0700

    change titles

--- src/problem2/src/components/App.tsx
+++ src/problem2/src/components/App.tsx
@@ -3,7 +3,7 @@ import { Converter } from "@components/Converter";
 const App = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-10 text-6xl font-medium">Crypto Exchanger</h1>
+      <h1 className="mb-10 text-6xl font-medium">Crypto Converter</h1>
 
       <Converter />
     </div>
