eb5f2a899b403b843365b82046c90a480a259b4f
PhatTran <phat.th@ooolab.edu.vn>
2025-02-06 21:23:24 +0700

    update solution

--- src/problem1/index.js
+++ src/problem1/index.js
@@ -3,7 +3,7 @@
 //**Output**: `return` - summation to `n`, i.e. `sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15`.
 
 var sum_to_n_a = function(n) {
-  const sum = 0;
+  let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += i;
   }
@@ -19,3 +19,7 @@ var sum_to_n_c = function(n) {
   const sum = Array.from({ length: n }, (_, i) => i + 1).reduce((a, b) => a + b, 0);
   return sum;
 };
+
+console.log("sum_to_n_a(5):", sum_to_n_a(1000));
+console.log("sum_to_n_b(5):", sum_to_n_b(1000)); 
+console.log("sum_to_n_c(5):", sum_to_n_c(1000));
