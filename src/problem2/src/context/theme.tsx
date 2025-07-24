6126d3604171aa0bc0bc748458b96352558bec1b
Nhut Ngo <nhut.ngo@vnsilicon.net>
2025-07-25 00:57:31 +0700

    theme key

--- src/problem2/src/context/theme.tsx
+++ src/problem2/src/context/theme.tsx
@@ -1,6 +1,8 @@
 import { Theme } from '@radix-ui/themes'
 import { createContext, useCallback, useState } from 'react'
 
+const THEME_KEY = window.location?.host + '-isDarkMode'
+
 interface ThemeContextType {
   isDarkMode: boolean
   toggleDarkMode: () => void
@@ -10,7 +12,7 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [isDarkMode, setIsDarkMode] = useState(() => {
-    const curIsDarkMode = localStorage.getItem('isDarkMode')
+    const curIsDarkMode = localStorage.getItem(THEME_KEY)
     document.documentElement.setAttribute('data-theme', curIsDarkMode ? 'dark' : 'light')
     return curIsDarkMode === 'true'
   })
@@ -18,7 +20,7 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const toggleDarkMode = useCallback(() => {
     setIsDarkMode((prev) => {
       const newIsDarkMode = !prev
-      localStorage.setItem('isDarkMode', newIsDarkMode.toString())
+      localStorage.setItem(THEME_KEY, newIsDarkMode.toString())
       document.documentElement.setAttribute('data-theme', newIsDarkMode ? 'dark' : 'light')
       return newIsDarkMode
     })
